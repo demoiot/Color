9fff26e4c777ea6cfeac59c88e86b37fd0eb6f1f
demoiot <49246711+demoiot@users.noreply.github.com>
2019-10-26 02:49:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,9 @@ modified from chengengyue
 //% color="#228B22" weight=25 icon="\uf0b2"
 namespace CrocoKit_Sensor {
 
-    let COLOR_ADD: number;
+    //let COLOR_ADD: number;
     //const COLOR_ADD = 0X53;
+    const COLOR_ADD = 39;
     const COLOR_REG = 0x00;
     const COLOR_R = 0X10;
     const COLOR_G = 0X0D;
